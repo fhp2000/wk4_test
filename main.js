148db1afd46140dbe7329aef77f5c71519082a96
fhp2000 <145046982+fhp2000@users.noreply.github.com>
2023-11-06 19:51:26 +0400

    ok

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://fhp2000.github.io/fhp2000.github.io/cities1.json');
+ourRequest.open('GET', 'https://fhp2000.github.io/wk4_test.github.io/cities1.json');
 ourRequest.onload = function() {
   var ourData = ourRequest.responseText
  console.log(ourData[0]);
